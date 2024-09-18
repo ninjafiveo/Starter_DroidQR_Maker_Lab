b945822c2eb5d89217fcba89af94b79680fa2eb6
ColtonSugar <coltonsugar123@gmail.com>
2024-09-18 11:33:54 -0400

    QR Code for droids

--- DroidQR_Maker_Lab/DroidQR.cs
+++ DroidQR_Maker_Lab/DroidQR.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QRCoder;
+using System.Drawing;
+using System.IO;
 
 namespace DroidQR_Maker_Lab
 {
@@ -18,8 +21,20 @@ namespace DroidQR_Maker_Lab
         // Placeholder for students to implement the QR Code generation logic
         public void GenerateQRCode()
         {
-            // TODO: Implement QR Code generation and saving functionality
-            Console.WriteLine($"QR Code for Droid {DroidID} has been generated.");
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qRCodeData = qrGenerator.CreateQrCode(DroidID, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qRCodeData);
+
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            string folderpath = Path.Combine(Directory.GetCurrentDirectory(), "QR_Codes");
+            Directory.CreateDirectory(folderpath);
+
+            string fileName = $"Droid_{DroidID}_QR.png";
+            string filePath = Path.Combine(folderpath, fileName);
+
+            qrCodeImage.Save(filePath);
+            Console.WriteLine($"QR Code for droid {DroidID} saved to {filePath}");
         }
     }
 }
