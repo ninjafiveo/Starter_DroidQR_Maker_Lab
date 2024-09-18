6810be3ac7ec03d76561c4bb955fe631f4fbace8
mick donalds <clay.thompson8172006@gmail.com>
2024-09-18 11:11:25 -0400

    Implemented QR code generation for droids.

--- DroidQR_Maker_Lab/DroidQR.cs
+++ DroidQR_Maker_Lab/DroidQR.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QRCoder;
+using System.IO;
+using System.Drawing;
+
 
 namespace DroidQR_Maker_Lab
 {
@@ -19,6 +23,19 @@ namespace DroidQR_Maker_Lab
         public void GenerateQRCode()
         {
             // TODO: Implement QR Code generation and saving functionality
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(DroidID, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            string FolderPath = Path.Combine(Directory.GetCurrentDirectory(), "QR_Codes");
+
+            string fileName = $"Droid_{DroidID}_QR.png";
+            string filePath = Path.Combine(FolderPath, fileName);
+
+            qrCodeImage.Save(filePath);
+
             Console.WriteLine($"QR Code for Droid {DroidID} has been generated.");
         }
     }
