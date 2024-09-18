792a8437b4bbedf9ab4adf70f3d236a093a09b51
BreannaT07 <144352346+BreannaT07@users.noreply.github.com>
2024-09-18 11:54:05 -0400

    Update Program.cs

--- DroidQR_Maker_Lab/Program.cs
+++ DroidQR_Maker_Lab/Program.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using QRCoder;
-using System.Drawing;
-using System.IO;
 
 namespace DroidQR_Maker_Lab
 {
@@ -23,7 +20,7 @@ namespace DroidQR_Maker_Lab
                 string droidID = Console.ReadLine();
 
                 // Create QR Code (To be completed by students)
-                DroidQR_Maker_Lab.DroidQR droidQR = new DroidQR_Maker_Lab.DroidQR(droidID);
+                DroidQRMaker.DroidQR droidQR = new DroidQRMaker.DroidQR(droidID);
                 droidQR.GenerateQRCode();
 
                 // Ask if they want to generate another QR Code
